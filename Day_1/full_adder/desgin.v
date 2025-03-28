83d9c45d281c2f4af9aa00799fd476c9a3215fa2
suresh <154115507+suresh2327@users.noreply.github.com>
2025-03-28 18:25:35 +0530

    Create desgin.v

---
+++ Day_1/full_adder/desgin.v
@@ -0,0 +1,10 @@
+module full_adder (
+    input a, 
+    input b, 
+    input cin, 
+    output sum, 
+    output cout
+);
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (a & cin);
+endmodule
