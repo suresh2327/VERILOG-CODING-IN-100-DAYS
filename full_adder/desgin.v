97cd0c89ad200fa42f637d11448ac933f5ee1fb0
suresh <154115507+suresh2327@users.noreply.github.com>
2025-03-28 18:40:28 +0530

    Delete full_adder directory

--- full_adder/desgin.v
+++
@@ -1,7 +0,0 @@
-//desgin code for full adder using data flow model
-module full_adder(a,b,c,sum,carry);
-input a,b,c;
-output sum,carry;
-assign sum = a^b^c;
-assign carry = (((a^b)&c)|(a&b));
-endmodule
