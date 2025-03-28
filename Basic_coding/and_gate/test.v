ee10369e150dc7b2755d0aad1e9be2b413830602
suresh <154115507+suresh2327@users.noreply.github.com>
2025-03-28 18:53:05 +0530

    Create test.v

---
+++ Basic_coding/and_gate/test.v
@@ -0,0 +1,21 @@
+module and_gate_tb;
+    reg a, b;
+    wire y;
+
+    and_gate uut (
+        .a(a), 
+        .b(b), 
+        .y(y)
+    );
+
+    initial begin
+        $monitor("a=%b b=%b | y=%b", a, b, y);
+        
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+
+        $stop;
+    end
+endmodule
