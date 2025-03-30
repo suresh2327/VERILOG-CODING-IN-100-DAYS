a58a5aa295da54abea60e95c3f4e5b6debffaacc
suresh <154115507+suresh2327@users.noreply.github.com>
2025-03-30 20:07:48 +0530

    Create desgin.v

---
+++ Day_7/binary to gray code converter/desgin.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+module binarytogray( 
+        input [3:0]b,
+        output reg [3:0]g );
+        always@(*)
+            begin
+             g[3]=b[3];
+              g[2]=b[3]^b[2];
+              g[1]=b[2]^b[1];
+              g[0]=b[1]^b[0];
+end
+endmodule
