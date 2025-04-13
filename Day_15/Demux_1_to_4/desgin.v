ec63f06fc68eacb29cfb9a9596e9ff5f930e3065
suresh <154115507+suresh2327@users.noreply.github.com>
2025-04-13 21:58:00 +0530

    Create desgin.v

---
+++ Day_15/Demux_1_to_4/desgin.v
@@ -0,0 +1,18 @@
+//desgin code for demux 1 to 4
+module demux_1to4 (
+    input wire din,
+    input wire [1:0] sel,
+    output reg [3:0] dout
+);
+
+always @(*) begin
+    dout = 4'b0000;
+    case (sel)
+        2'b00: dout[0] = din;
+        2'b01: dout[1] = din;
+        2'b10: dout[2] = din;
+        2'b11: dout[3] = din;
+    endcase
+end
+
+endmodule
