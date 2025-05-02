21f6fadfc2f2c98d0132f68b3927b7f5d2a52728
suresh <154115507+suresh2327@users.noreply.github.com>
2025-05-02 20:01:43 +0530

    desgin.v

---
+++ Day_29/counter_with_enable/desgin.v
@@ -0,0 +1,15 @@
+module basic_counter (
+    input clk,
+    input reset,
+    input enable,
+    output reg [3:0] count
+);
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            count <= 4'b0000;
+        else if (enable)
+            count <= count + 1;
+    end
+
+endmodule
